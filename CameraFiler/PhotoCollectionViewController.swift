123cb0ad74ad381982c36154f81292a01b5c72b7
Oleg Kudimov <ovkudimov@gmail.com>
2022-11-24 18:41:46 +0300

    initial commit

---
+++ CameraFiler/PhotoCollectionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoCollectionViewController.swift
+//  CameraFiler
+//
+//  Created by Oleg Kudimov on 11/24/22.
+//
+
+import Foundation
+import UIKit
+
+class PhotoCollectionViewController: UICollectionViewController {
+    
+}
