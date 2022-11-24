699fb378f32ecf671b2f91e71c28060909e7c5bb
Oleg Kudimov <ovkudimov@gmail.com>
2022-11-24 13:15:35 +0300

    Initial Commit

---
+++ CameraFiler/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CameraFiler
+//
+//  Created by Oleg Kudimov on 11/24/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
